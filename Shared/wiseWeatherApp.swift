7e0447255dacf47fd0f3ed39add10e92704b46bd
Jason A. Wise <jasonandrewwise@gmail.com>
2022-02-07 08:53:31 -0500

    Initial Commit

---
+++ Shared/wiseWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  wiseWeatherApp.swift
+//  Shared
+//
+//  Created by Jason Wise on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct wiseWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
